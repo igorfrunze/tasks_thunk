ebd936f3ab2dc20f18adbcca2702bcaec74d97e8
igor_frunze <igor_frunze@live.com>
2024-12-20 13:26:57 +0300

    redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk';
 import { todosReducer, searchTaskReducer } from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const reducer = combineReducers({
-  
   todos: todosReducer,
   searchTask: searchTaskReducer,
 });
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+export const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
